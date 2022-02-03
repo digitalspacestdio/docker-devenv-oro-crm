5952bc81009005b16eb165a6bd8fc0fc9295ca4d
scherepanov <sergey@cherepanov.org.ua>
2022-02-04 01:00:34 +0400

    xdebug

--- docker/fpm/entrypoint.sh
+++ docker/fpm/entrypoint.sh
@@ -2,6 +2,11 @@
 set -e
 #set -x
 
+HOST_MACHINE_IP=$(getent hosts host.docker.internal | awk '{print $1}' || echo '172.17.0.1')
+HOST_MACHINE_IP=${HOST_MACHINE_IP:-'172.17.0.1'}
+cd $(readlink /home/linuxbrew/.linuxbrew/etc/php/current)
+sed 's/xdebug.client_host=.*/xdebug.client_host='$HOST_MACHINE_IP'/g' php.ini.dist > php.ini
+
 cd /var/www
 
 chown linuxbrew:linuxbrew /var/www 2> /dev/null || true
