c1eb60f462a97414b2a4fb87990353ba8eea02dc
scherepanov <sergey@cherepanov.org.ua>
2022-02-11 00:58:55 +0400

    mariadb

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
-  version "0.1.3"
+  version "0.1.4"
   revision 1
 
   depends_on 'coreutils'
