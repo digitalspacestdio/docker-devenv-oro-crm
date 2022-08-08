eae6442f72d92e902214f5d34a07a4a57e620f97
scherepanov <sergey@cherepanov.org.ua>
2022-08-08 16:03:40 +0300

    rootless support

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 set -e
 
+# OWNER_UID=$(stat -c '%u' /var/www)
+# OWNER_GID=$(stat -c '%g' /var/www)
+
+# usermod -u $nginx_uid -o www-data && groupmod -g $nginx_gid -o www-data
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
-
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ] || [ "$1" = 'symfony' ]; then
-	setfacl -R -m u:php:rwX -m u:"$(whoami)":rwX /var/www
-	setfacl -dR -m u:php:rwX -m u:"$(whoami)":rwX /var/www
-fi
-
-chown php:php /var/www 2> /dev/null || true
-chown php:php /home/php/.composer 2> /dev/null || true
-chown php:php /home/php/.npm 2> /dev/null || true
-chown php:php /proc/self/fd/1
-chown php:php /proc/self/fd/2
 
 for i in "$@"; do
     i="${i//\\/\\\\}"
@@ -23,4 +17,17 @@ for i in "$@"; do
     C="$C \"${i//\"/\\\"}\""
 done
 
-HOME=/home/php su -p php -- -c "exec docker-php-entrypoint $C"
+exec docker-php-entrypoint $C
+
+# if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ] || [ "$1" = 'symfony' ]; then
+# 	setfacl -R -m u:php:rwX -m u:"$(whoami)":rwX /var/www
+# 	setfacl -dR -m u:php:rwX -m u:"$(whoami)":rwX /var/www
+# fi
+
+# chown php:php /var/www 2> /dev/null || true
+# chown php:php /home/php/.composer 2> /dev/null || true
+# chown php:php /home/php/.npm 2> /dev/null || true
+# chown php:php /proc/self/fd/1
+# chown php:php /proc/self/fd/2
+
+# HOME=/home/php su -p php -- -c "exec docker-php-entrypoint $C"
