98fc52670f813ca168d35c79f173b7683ea3a0db
scherepanov <sergey@cherepanov.org.ua>
2022-08-15 14:49:49 +0300

    max_locks_per_transaction=10000 -c shared_buffers=512GB

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
-  version "0.4.8"
-  revision 5
+  version "0.4.9"
+  revision 1
 
   depends_on 'coreutils'
   depends_on 'rsync'
